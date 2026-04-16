e34b9785979377aea0ec9fce984853cce902de44
Prime Genesis <nguyenkhaica2006@gmail.com>
2026-04-16 11:41:43 +0700

    add: parser implementation

--- parser.py
+++ parser.py
@@ -32,5 +32,65 @@ class ParseError(Exception):
 
 
 def p_term(inp: str) -> Term:
-    # TODO
-    return Term()
+    tokens = inp.split()
+
+    def base94_to_int(s: str) -> int:
+        if not s:
+            raise ParseError("UnexpectedEOF")
+        val = 0
+        for c in s:
+            val = val * 94 + (ord(c) - 33)
+        return val
+
+    def decode_string(body: str) -> str:
+        return "".join(CHARS_DECODED[ord(c) - 33] for c in body)
+
+    def parse_next() -> Term:
+        token = tokens.pop(0)
+        indicator = token[0]
+        body = token[1:]
+        if indicator == "T":
+            return TBool(True)
+        elif indicator == "F":
+            return TBool(False)
+        elif indicator == "I":
+            if not body:
+                raise ParseError("UnexpectedEOF")
+            return TInt(base94_to_int(body))
+        elif indicator == "S":
+            if not body:
+                raise ParseError("UnexpectedEOF")
+            return TString(decode_string(body))
+        elif indicator == "U":
+            if not body:
+                raise ParseError("UnexpectedEOF")
+            return TUnOp(body, parse_next())
+        elif indicator == "B":
+            if not body:
+                raise ParseError("UnexpectedEOF")
+            left = parse_next()
+            right = parse_next()
+            return TBinOp(left, body, right)
+        elif indicator == "?":
+            condition = parse_next()
+            true_branch = parse_next()
+            false_branch = parse_next()
+            return TIf(condition, true_branch, false_branch)
+        elif indicator == "L":
+            if not body:
+                raise ParseError("UnexpectedEOF")
+            variable = base94_to_int(body)
+            term = parse_next()
+            return TLam(variable, term)
+        elif indicator == "v":
+            if not body:
+                raise ParseError("UnexpectedEOF")
+            variable = base94_to_int(body)
+            return TVar(variable)
+        else:
+            raise ParseError("UnexpectedChar", index=0, ch=indicator)
+
+    term = parse_next()
+    if tokens:
+        raise ParseError("UnusedInput", index=0)
+    return term
